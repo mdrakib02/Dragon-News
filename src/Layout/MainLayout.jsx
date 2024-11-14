955a5344afea33855466e1de6e24f67abfa1cdb9
mdrakib02z <mdrakibahmed515@gmail.comm>
2024-11-14 17:04:06 +0600

    added main side bar sections

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -15,7 +15,11 @@ export default function MainLayout() {
         <nav className="w-10/12 mx-auto pt-12">
           <NavBar></NavBar>
         </nav>
-        <main></main>
+        <main className="w-10/12 mx-auto grid grid-cols-1 md:grid-cols-12 items-center pt-12">
+          <aside className="col-span-3">Left side bar</aside>
+          <section className="col-span-6">main content</section>
+          <aside className="col-span-3">Right side bar</aside>
+        </main>
       </div>
     </>
   );
