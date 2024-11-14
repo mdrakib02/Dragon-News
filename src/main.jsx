f567c694c7d771a10dde7236d7c387fcb06efbfe
mdrakib02z <mdrakibahmed515@gmail.comm>
2024-11-14 21:23:41 +0600

    fixed routes issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Router/Routes'
+import { RouterProvider, Routes } from 'react-router-dom'
+import router from './Routes/Routes'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
